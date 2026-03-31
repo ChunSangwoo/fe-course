e5a16b8cad30e89bf49d9ecec0a5202b6d2695f1
ChunSangwoo <aksyd23@gmail.com>
2026-03-31 18:00:19 +0900

    03.31 수업

---
+++ javascript/13.asyncronuos/4_promise2.js
@@ -0,0 +1,31 @@
+/**
+ * async / await
+ * - 비동기식 로직의 순차적인 순서를 보장하는 키워드
+ * - async는 await를 포함한 함수 앞에 붙임
+ * - await는 실행되는 비동기 객체 앞에 붙임
+ */
+
+async function promiseTest() {
+  let dataArray = null;
+
+  //promise 객체 생성
+  let promise1 = new Promise((resolve, reject) => {
+    let numbers = [1, 2, 3, 4, 5];
+    resolve(numbers);
+  });
+
+  await promise1
+    .then((result) => {
+      dataArray = result;
+      //   console.log("result => ", result);
+    })
+    .catch((error) => {
+      console.log("error => ", error);
+    });
+
+  console.log("dataArray = ", dataArray);
+}
+
+promiseTest();
+
+//async랑 await는 쌍으로 나와야함. then catch도 쌍으로 나와야함
