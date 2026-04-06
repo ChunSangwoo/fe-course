417b9b2d0115dee034d1bad8ca90b59de96a5c83
ChunSangwoo <aksyd23@gmail.com>
2026-04-06 16:03:37 +0900

    04.06 수업

--- cgv-demo/js/cgv_support.js
+++ cgv-demo/js/cgv_support.js
@@ -15,29 +15,49 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  /** 테이블 생성 함수 */
-  // createTable();
-  filterMenu("all");
+  filterMenu("all"); //맨 처음 호출되는 테이블
+
+  let menuList = document.querySelectorAll(".filter-menu li a");
+  menuList.forEach((menu) => {
+    menuList.forEach((menu) =>
+      menu.id === "all" ? menuSelectColor(menu) : menuDefaultColor(menu),
+    );
+
+    menu.addEventListener("click", () => {
+      menuList.forEach((menu) => menuDefaultColor(menu));
+      menuSelectColor(menu);
+      filterMenu(menu.id);
+    });
+  });
 }); //window event
 
-// filterMenu 함수 생성
+function menuDefaultColor(menu) {
+  menu.style.background = "rgb(137,137,135)";
+  menu.style.borderLeft = `1px solid var(rgb(137,137,135))`;
+  menu.style.borderBottom = `2px solid var(rgb(137,137,135))`;
+}
+
+function menuSelectColor(menu) {
+  menu.style.background = "rgb(251, 67, 87)";
+  menu.style.borderLeft = `1px solid var(rgb(251, 67, 87))`;
+  menu.style.borderBottom = `2px solid var(rgb(251, 67, 87))`;
+}
+
+//filterMenu 함수 생성
 async function filterMenu(type) {
-  console.log(type);
   let filterList = null;
+
   if (type === "all") {
-    // createTable();
     filterList = await getJson();
   } else {
     filterList = await filterData(type);
   }
-  // return filterList;
-  createTable(filterList); //맨 처음 호출되는 테이블
+  createTable(filterList);
 }
 
-// filterData 함수 생성
+//filterData 함수 생성
 async function filterData(type) {
   let data = await getJson();
-  // console.log("data => ", data);
   return data.filter((item) => item.type === type);
 }
 
@@ -48,8 +68,6 @@ async function getJson() {
 }
 
 async function createTable(list) {
-  // let list = await getJson();
-  // console.log('list =>', list);
   let output = `
         <table id='stable'>
             <thead>
